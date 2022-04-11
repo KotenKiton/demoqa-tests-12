e7692dcc901a9e5e955f3143fd71f2b608c30f53
Koten <Kotenkiton@gmail.com>
2022-04-12 01:51:39 +0300

    Junit 5

---
+++ src/test/java/guru/qa/SimpleTest.java
@@ -0,0 +1,50 @@
+package guru.qa;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.*;
+
+public class SimpleTest {
+
+    @BeforeAll
+    static void initDB(){
+        System.out.println("### @BeforeAll");
+    }
+
+    @BeforeEach
+    void openYaPage(){
+        System.out.println("###  @BeforeEach");
+       // Selenide.open("https://ya.ru");
+    }
+    @AfterEach
+    void close(){
+        System.out.println("###  @AfterEach");
+       // WebDriverRunner.closeWindow();
+    }
+    @AfterAll
+    static void cleanDB(){
+        System.out.println("### @AfterAll");
+
+    }
+
+    @Test
+    void assertTest() {
+        System.out.println("###               @test 0");
+        //... поиск в новостях
+    }
+    @Test
+    void assertTest1() {
+        System.out.println("###               @test 1");
+        // поиск в картинках
+    }
+    @Test
+    void assertTest2() {
+        System.out.println("###               @test 2");
+        // поиск в картинках
+    }
+    @Test
+    void assertTest3() {
+        System.out.println("###               @test 3");
+        // поиск в картинках
+    }
+}
